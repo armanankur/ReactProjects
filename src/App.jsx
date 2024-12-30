37aed129b46d320cb3eb4d7af49eb0648972caf0
Ankur Anand <armanankur@gmail.com>
2024-12-30 16:27:35 +0530

    ReactProject template

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import Todo from "./components/Todo";
-// import ToDoList from "./components/ToDoList";
+
 
 function App() {
   return (
     <>
-      <Todo />
-      {/* <ToDoList/> */}
+     
     </>
   );
 }
